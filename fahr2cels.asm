83d5e2601f9c9f4462f9315cea5e94fc3634acfe
Ian Macalinao <me@ian.pw>
2015-02-05 09:36:25 -0600

    Fahrenheit to celsius

---
+++ fahr2cels.asm
@@ -0,0 +1,19 @@
+	.data
+fahr: .float 43.5
+fp1: .float 5.0
+fp2: .float 9.0
+fp3: .float 32.0
+
+	.text
+l.s $f16, fp1
+l.s $f18, fp2
+div.s $f17, $f16, $f18 # Coefficient
+l.s $f15, fahr
+l.s $f16, fp3
+sub.s $f18, $f15, $f16
+mul.s $f12, $f17, $f18
+
+li $v0, 2 # print float
+syscall
+# 6.389
+	
